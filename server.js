41e8c376b31fd1e5cc90c87d47f1f150145bcd42
Pranav Kapoor <pranav85kapoor@gmail.com>
2026-01-01 17:48:09 +0530

    Add server.js

--- server.js
+++ server.js
@@ -16,3 +16,6 @@ app.use("/api/auth", require("./routes/authRoutes"));
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
+
+
+//inbz liij dqxb kxus
